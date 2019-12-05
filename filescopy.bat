dac7cb485197f70a724b648216987f897f96c153
csnmiraclesoft <48434382+csnmiraclesoft@users.noreply.github.com>
2019-12-05 16:48:48 +0530

    Update filescopy.bat

--- filescopy.bat
+++ filescopy.bat
@@ -1,1 +1,4 @@
-az storage file upload --account-name persistentvolumetest --account-key /d6uujYtWL85RNgSK4vkWmG6itcSiUAlBWFme6SO7uuG7CM7drnPa6fDBWG++8njpIRa5EQmwXfKyiLG4Fq6Ng== --share-name "routes" --source "/*.xml" --path "/*.xml"
+for i in `ls`
+do
+`az storage file upload --account-name persistentvolumetest --account-key /d6uujYtWL85RNgSK4vkWmG6itcSiUAlBWFme6SO7uuG7CM7drnPa6fDBWG++8njpIRa5EQmwXfKyiLG4Fq6Ng== --share-name "routes" --source $i`
+done
