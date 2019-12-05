d2485868d9def59980c844ce2c8f0d6c2f9dd9ec
csnmiraclesoft <48434382+csnmiraclesoft@users.noreply.github.com>
2019-12-05 18:21:41 +0530

    Update filescopy.bat

--- filescopy.bat
+++ filescopy.bat
@@ -1,4 +1,4 @@
 for i in `ls`
 do
 `az storage file upload --account-name persistentvolumetest --account-key /d6uujYtWL85RNgSK4vkWmG6itcSiUAlBWFme6SO7uuG7CM7drnPa6fDBWG++8njpIRa5EQmwXfKyiLG4Fq6Ng== --share-name "routes" --source $i`
-done
+dones
